fe1af77d42ea7726a6ff554d975ae9eb806f7c23
whisky-lover <s3877698@rmit.edu.vn>
2022-06-12 20:34:06 +0700

    responsive onboarding

--- src/components/OnBoarding/ButtonLarge.js
+++ src/components/OnBoarding/ButtonLarge.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const ButtonLarge = ({ title }) => {
 	return (
-		<button className="flex items-center justify-center w-4/5 px-16 py-5 text-2xl font-bold capitalize transition-all duration-300 border-none rounded-full shadow-lg outline-none cursor-pointer select-none hover:-translate-y-2 shadow-stone-800 text-lightGray bg-brown lg:w-1/4">
+		<button className="flex items-center justify-center w-4/5 px-16 py-5 text-2xl font-bold capitalize transition-all duration-300 border-none rounded-full shadow-lg outline-none cursor-pointer select-none md:py-7 md:text-3xl hover:-translate-y-2 shadow-stone-800 text-lightGray bg-brown lg:w-1/4 md:landscape:w-1/2">
 			<Link to="/home">{title}</Link>
 		</button>
 	);
--- src/components/OnBoarding/OverlayOnBoarding.js
+++ src/components/OnBoarding/OverlayOnBoarding.js
@@ -6,10 +6,10 @@ const OverlayOnBoarding = () => {
 	return (
 		<div className="absolute inset-0 z-10 text-white bg-black bg-opacity-40">
 			<div className="flex space-y-6 h-full flex-col px-5 py-[120px] justify-end items-center">
-				<h3 className="text-gray font-bold leading-[60px] text-4xl">
+				<h3 className="text-gray font-bold leading-[60px] text-4xl md:text-6xl lg:text-8xl lg:leading-[120px] md:leading-[80px] md:landscape:leading-[90px] md:landscape:text-7xl">
 					Good coffee, <br /> Good friends, <br /> let it blends
 				</h3>
-				<p className="text-lightGray w-[65%] text-lg text-center leading-8 font-[300]">
+				<p className="text-lightGray w-[65%] lg:w-[50%] md:py-4 md:landscape:py-2 lg:py-6 text-lg md:text-3xl md:landscape:text-3xl lg:text-4xl text-center leading-8 font-[300]">
 					The best grain, the finest roast, the most powerful flavor.
 				</p>
 
