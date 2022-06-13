c74a39f92cfb7c8a6bee7cca5e6dfc65e7eed1f3
whisky-lover <s3877698@rmit.edu.vn>
2022-06-13 07:34:57 +0700

    responsive onboarding

--- src/views/OnBoarding.js
+++ src/views/OnBoarding.js
@@ -4,7 +4,7 @@ import "./OnBoarding.css";
 
 const OnBoarding = () => {
 	return (
-		<div className="container relative mx-auto">
+		<div className="container relative mx-auto lg:max-w-none">
 			<OverlayOnBoarding />
 
 			<div className="relative lg:blur-none blur-[2px] on-boarding bg-cover bg-no-repeat bg-center min-h-screen"></div>
